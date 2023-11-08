b97814f2f9643e70842bf17a056762465742494e
aHmed <benezzine.ahmed@esprit.tn>
2023-11-08 17:39:39 +0100

    found a solution for comparing the encrypted passsword in the signin function

--- controllers/User.js
+++ controllers/User.js
@@ -7,37 +7,30 @@ import bcrypt from 'bcrypt';
 
 
 //signin function
+
 export function signin(req, res) {
-    User
-    .findOne({ "email": req.body.email, "password": req.body.password })
-    .then(doc => {
-        res.status(200).json(doc);
-        res.json({messsage: 'successfully signed in'})
+  const { email, password } = req.body;
+
+  User
+    .findOne({ "email": req.body.email })
+    .then(async doc => {
+      if (!doc) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const isValidPassword = await bcrypt.compare(password, doc.password);
+      if (!isValidPassword) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      res.status(200).json(doc);
     })
     .catch(err => {
-        res.status(500).json({ error: err });
+      res.status(500).json({ error: err });
     });
 }
 
-
 //signup function 
-/*export function signup(req, res) {
-    User.create(req.body)
-    .then((newUser) => {
-        res.status(201).json({
-            email:newUser.email,
-            password:newUser.password,
-            phone_number:newUser.Phone_number,
-            role:newUser.role,
-            name:newUser.name,
-            first_name:newUser.first_name,
-            age:newUser.age
-        });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-}*/
 export async function signup(req, res) {
   const { email, password, Phone_number, role, name, first_name, age } = req.body;
 
